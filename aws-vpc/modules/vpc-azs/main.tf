a45d305869c1304150e4c699430ba95574136a2e
Muhammad Asim <quickbooks2018@gmail.com>
2019-07-23 20:23:11 +0500

    Learning Terraform v12

---
+++ aws-vpc/modules/vpc-azs/main.tf
@@ -0,0 +1,112 @@
+provider "aws" {
+  region = "${var.region}"
+}
+
+resource "aws_vpc" "vpc" {
+  cidr_block = "${var.vpc-cidr}"
+  instance_tenancy = "${var.instance-tenancy}"
+  enable_dns_support = "${var.enable-dns-support}"
+  enable_dns_hostnames = "${var.enable-dns-hostnames}"
+
+  tags = {
+    Name= "${var.vpc-name}"
+    Location= "${var.vpc-location}"
+  }
+}
+
+#CREATING A INTERNET GATEWAY
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = "${aws_vpc.vpc.id}"
+
+  tags = {
+    Name= "${var.internet-gateway-name}"
+
+  }
+}
+
+#PUBLIC SUBNET FROM A LIST
+resource "aws_subnet" "public-subnets" {
+  availability_zone = "${element(var.azs,count.index)}"
+  count = "${length(var.vpc-public-subnet-cidr)}"
+  cidr_block = "${element(var.vpc-public-subnet-cidr,count.index)}"
+  vpc_id = "${aws_vpc.vpc.id}"
+  map_public_ip_on_launch = "${var.map_public_ip_on_launch}"
+
+  tags = {
+    Name = "${var.public-subnets-name}-${count.index+1}"
+    Location = "${var.public-subnets-location}"
+  }
+}
+
+#CREATING A PUBLIC ROUTES
+resource "aws_route_table" "public-routes" {
+  vpc_id = "${aws_vpc.vpc.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw.id}"
+  }
+  tags = {
+    Name = "${var.public-subnet-routes-name}"
+  }
+}
+
+#ASSOCIATE/LINK PUBLIC-ROUTE WITH PUBLIC-SUBNETS LIST
+resource "aws_route_table_association" "public-association" {
+  count = "${length(var.azs)}"
+  route_table_id = "${aws_route_table.public-routes.id}"
+  subnet_id = "${element(aws_subnet.public-subnets.*.id, count.index)}"
+}
+
+#CREATING PRIVATE SUBNETS FROM A LIST
+resource "aws_subnet" "private-subnets" {
+  availability_zone = "${element(var.azs,count.index)}"
+  count = "${length(var.vpc-private-subnet-cidr)}"
+  cidr_block = "${element(var.vpc-private-subnet-cidr,count.index)}"
+  vpc_id = "${aws_vpc.vpc.id}"
+
+
+  tags = {
+    Name = "${var.private-subnet-name}-${count.index+1}"
+    Location = "${var.private-subnets-location-name}"
+  }
+}
+
+#CREATING EIP NAT-GATEWAY FOR NAT-GATEWAY REDUNDANCY
+resource "aws_eip" "eip-ngw" {
+  count = "${var.total-nat-gateway-required}"
+  tags = {
+    Name = "${var.eip-for-nat-gateway-name}-${count.index+1}"
+  }
+}
+#CREATING NAT GATEWAYS IN PUBLIC-SUBNETS, EACH NAT-GATEWAY WILL BE DIFFERENT AZ FOR REDUNDANCY.
+resource "aws_nat_gateway" "ngw" {
+  count = "${var.total-nat-gateway-required}"
+  # count = "${length(var.azs)}"
+  allocation_id = "${element(aws_eip.eip-ngw.*.id,count.index)}"
+  subnet_id = "${element(aws_subnet.public-subnets.*.id, count.index)}"
+  tags = {
+    Name = "${var.nat-gateway-name}-${count.index+1}"
+  }
+}
+
+#CREATING A PRIAVTE ROUTE-TABLE FOR PRIVATE-SUBNETS
+resource "aws_route_table" "private-routes" {
+  count = "${length(var.azs)}"
+  vpc_id = "${aws_vpc.vpc.id}"
+  route {
+    cidr_block = "${var.private-route-cidr}"
+    nat_gateway_id = "${element(aws_nat_gateway.ngw.*.id,count.index)}"
+  }
+  tags = {
+    Name = "${var.private-route-name}-${count.index+1}"
+  }
+
+}
+
+#ASSOCIATE/LINK PRIVATE-ROUTES WITH PRIVATE-SUBNETS
+resource "aws_route_table_association" "private-routes-linking" {
+  count = "${length(var.azs)}"
+  route_table_id = "${element(aws_route_table.private-routes.*.id,count.index)}"
+  subnet_id = "${element(aws_subnet.private-subnets.*.id,count.index)}"
+}
---
+++ aws-vpc/vpc-azs.tf
@@ -0,0 +1,26 @@
+module "vpc" {
+  source = "./modules/vpc-azs"
+  ###VPC###
+  instance-tenancy = "default"
+  enable-dns-support = "true"
+  enable-dns-hostnames = "true"
+  vpc-name = "Cloudelligent-vpc"
+  vpc-location = "Frankfurt"
+  region = "eu-central-1"
+  azs = ["eu-central-1a","eu-central-1b"]
+  internet-gateway-name = "Cloudelligent-igw"
+  map_public_ip_on_launch = "true"
+  public-subnets-name = "public-subnets"
+  public-subnets-location = "Frankfurt"
+  public-subnet-routes-name = "public-subnet-routes"
+  private-subnets-location-name = "Frankfurt"
+  private-subnet-name = "private-subnets"
+  total-nat-gateway-required = "1"
+  eip-for-nat-gateway-name = "eip-nat-gateway"
+  nat-gateway-name = "nat-gateway"
+  private-route-cidr = "0.0.0.0/0"
+  private-route-name = "private-route"
+  vpc-cidr = "10.11.0.0/16"
+  vpc-public-subnet-cidr = ["10.11.1.0/24","10.11.2.0/24"]
+  vpc-private-subnet-cidr = ["10.11.4.0/24","10.11.5.0/24"]
+}
