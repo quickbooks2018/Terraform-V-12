05d608930640f9b9ddd439f7d6754026f347e115
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2020-06-25 09:04:05 +0500

    Terraform v12 by cloudgeeks.ca

--- terraform.v12/vpc+application-load-balancer/environment/dev/main.tf
+++ terraform.v12/vpc+application-load-balancer/environment/dev/main.tf
@@ -27,7 +27,7 @@ module "alb-sg" {
   namespace           = "cloudgeeks.ca"
   stage               = "dev"
   name                = "ALB"
-  tcp_ports           = "80,443"
+  tcp_ports           = "80,443,8080"
   cidrs               = ["0.0.0.0/0"]
   security_group_name = "Application-LoadBalancer"
   vpc_id              = module.vpc.vpc-id
