925804c32998fdc33654f36638857b2f45f64c45
Your Name <you@example.com>
2020-08-27 14:20:09 +0500

    Terraform v12 vpc

---
+++ terraform.v12/aws/modules/aws-vpc/environment/dev/main.tf
@@ -0,0 +1,27 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+#####
+# Vpc
+#####
+
+module "vpc" {
+  source = "../../modules/aws-vpc"
+
+  vpc-location                        = "Virginia"
+  namespace                           = "cloudgeeks.ca"
+  name                                = "vpc"
+  stage                               = "dev"
+  map_public_ip_on_launch             = "true"
+  total-nat-gateway-required          = "1"
+  create_database_subnet_group        = "false"
+  vpc-cidr                            = "10.11.0.0/16"
+  vpc-public-subnet-cidr              = ["10.11.1.0/24","10.11.2.0/24"]
+  vpc-private-subnet-cidr             = ["10.11.4.0/24","10.11.5.0/24"]
+  vpc-database_subnets-cidr           = ["10.11.7.0/24", "10.11.8.0/24"]
+}
+
+
+
+
