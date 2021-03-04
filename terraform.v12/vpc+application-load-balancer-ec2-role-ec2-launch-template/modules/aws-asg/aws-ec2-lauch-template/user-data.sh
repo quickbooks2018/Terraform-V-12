0736621e6b9dae1f310293d6f70f3898d6af35fc
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2021-03-04 07:51:36 +0500

    Update user-data.sh

--- terraform.v12/vpc+application-load-balancer-ec2-role-ec2-launch-template/modules/aws-asg/aws-ec2-lauch-template/user-data.sh
+++ terraform.v12/vpc+application-load-balancer-ec2-role-ec2-launch-template/modules/aws-asg/aws-ec2-lauch-template/user-data.sh
@@ -54,8 +54,6 @@ NODE_IP_ADDRESS="$ipV4"
 USE_LONGNAME=true
 " > /root/rabbitmq/rabbitmq-env.conf
 
-chmod 666 /root/rabbitmq/rabbitmq-env.conf
-
 cat > /root/rabbitmq/enabled_plugins <<'EOF'
 [rabbitmq_management,rabbitmq_peer_discovery_aws,rabbitmq_federation,rabbitmq_prometheus].
 EOF
