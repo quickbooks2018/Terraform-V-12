3e86cf30c2650886220b42aaae7a4806b73fe541
Muhammad Asim <36363841+quickbooks2018@users.noreply.github.com>
2022-03-27 08:33:21 +0500

    Update alb-ingress.sh

--- terraform.v12/terraform-vpc+eks+alb-ingress/environment/dev/alb-ingress.sh
+++ terraform.v12/terraform-vpc+eks+alb-ingress/environment/dev/alb-ingress.sh
@@ -1,92 +1,26 @@
 #!/bin/bash
-#Purpose: ALB Ingress Setup
-#Maintainer: Muhammad Asim <quickbooks2018@gmail.com>
+# Purpose: alb ingress setup via helm3
+# https://aws.amazon.com/premiumsupport/knowledge-center/eks-alb-ingress-controller-fargate/
+# https://github.com/aws/eks-charts
+# https://github.com/aws/eks-charts/tree/master/stable/aws-load-balancer-controller
 
-VPC_ID=`terraform output vpc-id`
-#ACCOUNT_ID=`aws sts get-caller-identity | grep -i account | cut -d '"' -f4`
-ACCOUNT_ID=`aws sts get-caller-identity | awk '{print $1}'`
+EKS_CLUSTER="cloudgeeks-eks-dev"
 REGION="us-east-1"
-CLUSTER_NAME="cloudgeeks.ca-dev-eks"
-POLICY_ARN=`aws sts get-caller-identity |  awk '{ print $2 }' | grep "iam" | cut -d':' -f2,3,4,5`
+AWS_ACCOUNT_ID="602401143452"
+# https://docs.aws.amazon.com/eks/latest/userguide/add-ons-images.html
+
+helm version --short
+
+helm repo add eks https://aws.github.io/eks-charts
+
+# To install the TargetGroupBinding custom resource definitions (CRDs), run the following command:
+
+helm repo update
+kubectl apply -k "github.com/aws/eks-charts/stable/aws-load-balancer-controller//crds?ref=master"
 
 
-# Create an IAM OIDC provider and associate it with your cluster. If you don't have eksctl version 0.14.0 or later installed, complete the instructions in Installing or Upgrading eksctl to install or upgrade it. You can check your installed version with eksctl version.
+# To install the Helm chart, run the following command:
 
-eksctl utils associate-iam-oidc-provider \
-    --region $REGION \
-    --cluster $CLUSTER_NAME \
-    --approve
+helm upgrade -i aws-load-balancer-controller eks/aws-load-balancer-controller -n kube-system --set serviceAccount.create=true --set region=${REGION} --set image.repository=${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/amazon/aws-load-balancer-controller --set clusterName=${EKS_CLUSTER}
 
-
-
-# Create an IAM policy called ALBIngressControllerIAMPolicy for the ALB Ingress Controller pod that allows it to make calls to AWS APIs on your behalf. Use the following AWS CLI command to create the IAM policy in your AWS account. You can view the policy document on GitHub.
-
-
-curl -# -LO https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/iam-policy.json
-
-sleep 10
-
-aws iam create-policy --policy-name ALBIngressControllerIAMPolicy --policy-document file://iam-policy.json
-
-
-
-
-# Create a Kubernetes service account named alb-ingress-controller in the kube-system namespace, a cluster role, and a cluster role binding for the ALB Ingress Controller to use with the following command. If you don't have kubectl installed, complete the instructions in Installing kubectl to install it.
-
- kubectl create serviceaccount alb-ingress-controller -n kube-system
- 
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/rbac-role.yaml
-
-#eksctl create iamserviceaccount --name alb-ingress-controller --namespace kube-system --cluster $CLUSTER_NAME --attach-policy-arn IAM-policy-arn --approve --override-existing-serviceaccounts
-
-# Create an IAM role for the ALB ingress controller and attach the role to the service account created in the previous step. If you didn't create your cluster with eksctl, then use the instructions on the AWS Management Console or AWS CLI tabs.
-
-# SEARCH ----> IAM ---> POLICY ---> ALBIngressControllerIAMPolicy ---> COPY THE ARN BELOW
-
-# https://stackoverflow.com/questions/47313778/find-role-being-used-on-server-from-aws-cli
-
-kubectl annotate serviceaccount -n kube-system alb-ingress-controller \
-eks.amazonaws.com/role-arn=arn:aws:iam::$ACCOUNT_ID:role/eks-alb-ingress-controller
-
-kubectl annotate serviceaccount -n kube-system alb-ingress-controller \
-eks.amazonaws.com/role-arn=arn:aws:iam::823629353122:role/eks-alb-ingress-controller
-
-
-
-# Create an IAM role for the ALB ingress controller and attach the role to the service account created in the previous step. If you didn't create your cluster with eksctl, then use the instructions on the AWS Management Console or AWS CLI tabs
-# https://docs.aws.amazon.com/eks/latest/userguide/alb-ingress.html
-
-# kubectl annotate serviceaccount -n kube-system alb-ingress-controller \
-# eks.amazonaws.com/role-arn=arn:aws:iam::$ACCOUNT_ID:role/alb-ingress-controller
-
-
-
-# Deploy the ALB Ingress Controller with the following command.
-
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.4/docs/examples/alb-ingress-controller.yaml
-
-
-echo -e "\nOpen the ALB Ingress Controller deployment manifest for editing with the following command\n"
-
-echo -e "\nkubectl edit deployment.apps/alb-ingress-controller -n kube-system\n"
-
-echo -e "\nThe line number is 41, at the  end of line press ENTER\n"
-
-echo -e "- --cluster-name=$CLUSTER_NAME"
-echo -e "- --aws-vpc-id=$VPC_ID"
-echo -e "- --aws-region=$REGION"
-
-
- 
-echo -e "\n If ALB is not setup check the logs with mentioned below commands \n"
-
-
-echo -e "\n    kubectl logs -n kube-system   deployment.apps/alb-ingress-controller   \n"
-    
-    
-echo -e "\n Confirm that the ALB Ingress Controller is running with the following command\n"
-
-echo -e "\n kubectl get pods -n kube-system \n"
-
-
-#END
+# END
